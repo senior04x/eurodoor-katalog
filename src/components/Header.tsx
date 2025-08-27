a560f904fef9d72aa562535bd3fdc8d64a274f17
senior04x <gcccc406@gmail.com>
2025-08-27 16:59:24 +0500

    ss

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -29,7 +29,7 @@ export default function Header({ currentPage, onNavigate }: HeaderProps) {
             <ImageWithFallback
               src="https://iili.io/K2WCLJV.png"
               alt="EURODOOR Logo"
-              className="h-20 w-auto"
+              className="h-10 w-auto"
             />
           </div>
 
